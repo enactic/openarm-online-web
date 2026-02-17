0b0ede9737d3b15144a79f9a49f096aa20c87c8a
Abe Tomoaki <abe@clear-code.com>
2026-02-17 15:58:04 +0900

    Change SQLALCHEMY_DATABASE_URI to return a string

--- app/settings.py
+++ app/settings.py
@@ -47,14 +47,14 @@ class Settings(BaseSettings):
     @computed_field  # type: ignore[prop-decorator]
     @property
     def SQLALCHEMY_DATABASE_URI(self) -> str:
-        return PostgresDsn.build(
+        return str(PostgresDsn.build(
             scheme="postgresql+psycopg",
             username=self.POSTGRES_USER,
             password=self.POSTGRES_PASSWORD,
             host=self.POSTGRES_SERVER,
             port=self.POSTGRES_PORT,
             path=self.POSTGRES_DB,
-        )
+        ))
 
 
 settings = Settings()
