f508b8da91494db368b2e15b17e5b7372e389819
Abe Tomoaki <abe@clear-code.com>
2026-03-05 12:03:52 +0900

    Add a script to create tasks (#31)

--- app/temporary-migration.sh
+++ app/temporary-migration.sh
@@ -13,17 +13,4 @@ rm -f alembic/versions/*.py
 alembic revision --autogenerate -m "wip"
 alembic upgrade head
 
-# Insert test dummy data.
-# todo: How to add tasks in production.
-python3 << CODE
-from sqlmodel import Session
-
-from app.db import engine
-from app.models import Task
-
-with Session(engine) as session:
-  session.add(Task(name="task1", prompt="dummy1", reset_docker_tag="reset tag1"))
-  session.add(Task(name="task2", prompt="dummy2", reset_docker_tag="reset tag2"))
-  session.add(Task(name="task3", prompt="dummy3", reset_docker_tag="reset tag3"))
-  session.commit()
-CODE
+../scripts/create_tasks.py tests/fixtures/task.jsonl
