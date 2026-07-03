9999d4bdaa91eadc8005632ba8d4aaed8b5f96ba
Abe Tomoaki <abe@clear-code.com>
2026-07-03 10:09:51 +0900

    infra: add a custom domain for the ALB (#103)

--- infra/modules/alb/outputs.tf
+++ infra/modules/alb/outputs.tf
@@ -15,3 +15,11 @@
 output "target_group_arn" {
   value = aws_lb_target_group.this.arn
 }
+
+output "dns_name" {
+  value = aws_lb.this.dns_name
+}
+
+output "zone_id" {
+  value = aws_lb.this.zone_id
+}
